f900113df066ebc8dafdd7ec9e7d8ca7066dd2ec
Crixto99 <93042230+Crixto99@users.noreply.github.com>
2021-12-06 03:40:18 -0600

    Update index.ts

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -10,21 +10,8 @@ export function runAll(client: Client) {
     slashHandler(client, fs, path);
     eventHandler(client, fs, path);
 
-    process.on("unhandledRejection", (reason, p) => {
-        console.log(" [antiCrash] :: Unhandled Rejection/Catch");
-        console.log(reason, p);
+    process.on("unhandledRejection", (e) => {
+       console.log(e);
     });
-    process.on("uncaughtException", (err, origin) => {
-        console.log(" [antiCrash] :: Uncaught Exception/Catch");
-         console.log(err, origin);
-    });
-    process.on("uncaughtExceptionMonitor", (err, origin) => {
-        console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
-        console.log(err, origin);
-    });
-    process.on("multipleResolves", (type, promise, reason) => {
-        console.log(" [antiCrash] :: Multiple Resolves");
-         console.log(type, promise, reason);
-    });
-
+    
 }
