f8dcc309967784e183e061bc7c3c9ce414b65290
Crixto99 <93042230+Crixto99@users.noreply.github.com>
2021-12-06 03:38:15 -0600

    Update ping.ts

--- src/Interaction/info/ping.ts
+++ src/Interaction/info/ping.ts
@@ -1,6 +1,6 @@
 module.exports = {
     name: "ping",
-    description: "Ver el ping del bot",
+    description: "El pepe",
     run: async(client, interaction) => {
         interaction.reply({ content: `Pong! \`${client.ws.ping}ms\``, ephemeral: true })
     }
