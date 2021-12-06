dfc4e96e3ecd15afe030d382ac36314527b1a6b7
Crixto99 <93042230+Crixto99@users.noreply.github.com>
2021-12-06 03:34:36 -0600

    Delete src/Commands/cmd directory

--- src/Commands/cmd/ping.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Puedes ver el ping del bot",
-    run: async(client, message, args, prefix) => {
-        message.reply(`Pong! \`${client.ws.ping}\``)
-    }
-}
